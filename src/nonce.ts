f68ffa08909e97a2e99fc2d3604443b95543e4c0
Andrew Heberle <andrew.heberle@gmail.com>
2025-11-20 14:30:43 +0800

    Shift Nonce into seperate file

---
+++ src/nonce.ts
@@ -0,0 +1,81 @@
+import { env } from "cloudflare:workers"
+import { ms } from "itty-time"
+import { Fingerprint, FingerprintFormatError, Key, parseFingerprint, parseSignature, Signature, SignatureParseError } from "sshpk"
+
+export class NonceParseError extends Error {
+    constructor(message: string, cause?: unknown) {
+        super(message)
+        this.name = "NonceParseError"
+        this.cause = cause
+
+        // This is necessary for proper stack trace in TypeScript
+        Object.setPrototypeOf(this, NonceParseError.prototype)
+    }
+}
+
+export class Nonce {
+    readonly timestamp: number
+    readonly fingerprint: Fingerprint
+    readonly signature: Signature
+    private readonly dataToVerify: string
+
+    constructor(nonce: string) {
+        const parts = nonce.split(".")
+        if (parts.length !== 3) {
+            throw new NonceParseError("invalid nonce format")
+        }
+
+        const [timestampStr, fingerprintHex, signatureBase64] = parts
+
+        // verify timestamp
+        const timestamp: number = parseInt(timestampStr, 10)
+        if (isNaN(timestamp)) {
+            throw new NonceParseError("timestamp was not a number")
+        }
+
+        const now = Date.now()
+        const age = now - timestamp
+        if (age > ms(env.CERTIFICATE_REQUEST_TIME_SKEW_MAX)) {
+            throw new NonceParseError("nonce timestamp too old")
+        }
+
+        // verify fingerprint matches public key
+        try {
+            const fingerprint = parseFingerprint(fingerprintHex)
+            if (fingerprint === undefined) {
+                throw new NonceParseError("nonce fingerprint did not parse")
+            }
+
+            // parse signature
+            const signature = parseSignature(signatureBase64, "ecdsa", "ssh")
+
+            // set our values
+            this.timestamp = timestamp
+            this.fingerprint = fingerprint
+            this.signature = signature
+            this.dataToVerify = `${timestamp}.${fingerprintHex}`
+        } catch (err) {
+            switch (true) {
+                case (err instanceof FingerprintFormatError):
+                    throw new NonceParseError("nonce fingerprint was an invalid format", err)
+                case (err instanceof SignatureParseError):
+                    throw new NonceParseError("nonce signature could not be parsed", err)
+                default:
+                    throw err
+            }
+        }
+    }
+
+    /**
+     * 
+     * @param key public key to use to verify fingerprint and signature against
+     * @returns true or false if verification succeeds
+     */
+    verify(key: Key) {
+        // create verifier from public key
+        const verifier = key.createVerify("sha256")
+        verifier.update(this.dataToVerify)
+
+        return verifier.verify(this.signature)
+    }
+}
