1f30e4c8829283dddf18529ee54aed6bda2bd416
Andrew Heberle <andrew.heberle@gmail.com>
2026-01-05 09:07:16 +0800

    More work on configure script

--- client/internal/pkg/tray/tray.go
+++ client/internal/pkg/tray/tray.go
@@ -336,8 +336,9 @@ func (app *Application) eventloop() {
 				if errors.Is(err, client.ErrAddingToAgent) || errors.Is(err, client.ErrConnectingToAgent) {
 					app.logger.Warn("certificate refreshed but could not add to agent", "error", err)
 					app.notify("Warning", "Certificate renewed byt could not add to SSH Agent", warningIcon)
+					break
 				}
-				// then do interactive renewal
+				// then try interactive renewal
 				app.logger.Warn("could not perform a refresh, running renew", "error", err)
 				if err := app.renew(); err != nil {
 					app.logger.Error("could not renew certificate", "error", err)
@@ -346,7 +347,7 @@ func (app *Application) eventloop() {
 				}
 			}
 
-			app.notify("Certificate Issued", "A new certificate was issued and added to the local ssh-agent", okIcon)
+			app.notify("Certificate Issued", "A new certificate was issued and added to the local SSH Agent", okIcon)
 			app.state = stateCertificateOK
 		case <-app.mGenerate.ClickedCh:
 			// start by disabling menu item so we aren't overlapping
