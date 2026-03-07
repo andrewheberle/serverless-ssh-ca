395b2b33e025fc0303a6352f4fe83c3cf27dc9f1
Andrew Heberle <andrewheberle@users.noreply.github.com>
2026-03-07 15:23:16 +0800

    Update env.d.ts

--- tests/env.d.ts
+++ tests/env.d.ts
@@ -1,6 +1,3 @@
 declare module "cloudflare:test" {
-  interface ProvidedEnv extends Env {
-    "SSH_CERTIFICATE_PRINCIPALS": "",
-	"SSH_CERTIFICATE_INCLUDE_SELF": "false"
-  }
+  interface ProvidedEnv extends Env {}
 }
