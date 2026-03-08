788d96cba6086e0935c3f95e101fc3344727f2c2
Andrew Heberle <andrewheberle@users.noreply.github.com>
2026-03-08 08:20:42 +0800

    Update request.test.ts

--- tests/request.test.ts
+++ tests/request.test.ts
@@ -1,5 +1,8 @@
-import { SELF } from "cloudflare:test"
+import { SELF, env, createExecutionContext, waitOnExecutionContext } from "cloudflare:test"
 import { describe, it, expect } from "vitest"
+import worker from "../src"
+
+const IncomingRequest = Request<unknown, IncomingRequestCfProperties>
 
 describe("get /404", () => {
     it ("responds with a 404", async () => {
@@ -17,7 +20,15 @@ describe("get /docs", () => {
 
 describe("get /api/v2/ca", () => {
     it ("responds with a 200", async () => {
-        const response = await SELF.fetch("http://example.com/api/v2/ca")
+		const request = new IncomingRequest("http://example.com/api/v2/ca")
+		
+		// Create an empty context to pass to `worker.fetch()`
+		const ctx = createExecutionContext()
+		const response = await worker.fetch(request, env, ctx)
+		
+		// Wait for all `Promise`s passed to `ctx.waitUntil()` to settle before running test assertions
+		await waitOnExecutionContext(ctx)
+		
         expect(response.status).toBe(200)
     })
 })
