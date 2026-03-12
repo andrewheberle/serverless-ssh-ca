d8894842968e6431359aefc3cb02402d2249a85a
Andrew Heberle <andrewheberle@users.noreply.github.com>
2026-03-12 21:51:09 +0800

    Update protect_windows.go

--- client/pkg/protect/protect_windows.go
+++ client/pkg/protect/protect_windows.go
@@ -1,25 +1,5 @@
 package protect
 
-import (
-	"golang.zx2c4.com/wireguard/windows/conf/dpapi"
-)
-
-// Decrypt will decrypt the secret called "name" using the Windows DPAPI
-func Decrypt(data []byte, name string) ([]byte, error) {
-	return dpapi.Decrypt(data, name)
-}
-
-// Encrypt will encrypt the secret called "name" using the Windows DPAPI
-func Encrypt(data []byte, name string) ([]byte, error) {
-	return dpapi.Encrypt(data, name)
-}
-
-// Decrypt will decrypt the secret called "name" using the Windows DPAPI
-func (p *DefaultProtector) Decrypt(data []byte, name string) ([]byte, error) {
-	return Decrypt(data, name)
-}
-
-// Encrypt will encrypt the secret called "name" using the Windows DPAPI
-func (p *DefaultProtector) Encrypt(data []byte, name string) ([]byte, error) {
-	return Encrypt(data, name)
+func NewDefaultProtector() Protector {
+	return &DpapiProtector{}
 }
