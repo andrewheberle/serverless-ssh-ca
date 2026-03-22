4687d7f9697e959aaa7b7383487ab11f0e2e5b0f
Andrew Heberle <andrewheberle@users.noreply.github.com>
2026-03-22 13:21:47 +0800

    Update util.go

--- client/internal/pkg/cli/util.go
+++ client/internal/pkg/cli/util.go
@@ -10,24 +10,6 @@ import (
 	"github.com/bep/simplecobra"
 )
 
-// loadsystemconfig will only attempt to load the system config file
-func loadsystemconfig(this *simplecobra.Commandeer) (*config.SystemConfig, error) {
-	// get root command for config locations
-	root, ok := this.Root.Command.(*rootCommand)
-	if !ok {
-		return nil, fmt.Errorf("problem accessing root command")
-	}
-
-	// load config
-	c, err := config.LoadConfig(root.systemConfigFile, "")
-	if err != nil {
-		return nil, err
-	}
-
-	// return system portion of config
-	return c.System(), nil
-}
-
 // loadconfig will load both system and user configuration
 func loadconfig(this *simplecobra.Commandeer) (*config.Config, error) {
 	// get root command for config locations
