eb4cf317d8697ac98e1fba74c44a571323395a0c
Andrew Heberle <andrew.heberle@gmail.com>
2026-04-10 15:16:28 +0800

    Some hardening of ProofOfPossession

--- src/proof.ts
+++ src/proof.ts
@@ -8,14 +8,14 @@ import { Sig } from "./sshsig/sig"
 const Namespace = "proof-of-possession@com.github.serverless-ssh-ca.andrewheberle"
 
 export class PossessionParseError extends Error {
-    constructor(message: string, cause?: unknown) {
-        super(message)
-        this.name = "PossessionParseError"
-        this.cause = cause
+	constructor(message: string, cause?: unknown) {
+		super(message)
+		this.name = "PossessionParseError"
+		this.cause = cause
 
-        // This is necessary for proper stack trace in TypeScript
-        Object.setPrototypeOf(this, PossessionParseError.prototype)
-    }
+		// This is necessary for proper stack trace in TypeScript
+		Object.setPrototypeOf(this, PossessionParseError.prototype)
+	}
 }
 
 /**
@@ -33,116 +33,122 @@ export class PossessionParseError extends Error {
  * in the certificate request and is a BASE64 encoded SSHSIG signature.
  */
 export class ProofOfPossession {
-    readonly timestamp: number
-    readonly fingerprint: Fingerprint
-    readonly signature: Sig
-    private readonly signaturePubkey: Key
-    private readonly data: string
+	readonly timestamp: number
+	readonly fingerprint: Fingerprint
+	readonly signature: Sig
+	readonly signaturePubkey: Key
+	private readonly data: string
 
-    constructor(proof: string, from?: number) {
-        const parts = proof.split(".")
-        if (parts.length !== 3) {
-            throw new PossessionParseError("invalid proof of possession format")
-        }
+	constructor(proof: string, from?: number) {
+		const parts = proof.split(".")
+		if (parts.length !== 3) {
+			throw new PossessionParseError("invalid proof of possession format")
+		}
 
-        const [timestampStr, fingerprintHex, signatureBase64] = parts
+		const [timestampStr, fingerprintString, signatureBase64] = parts
 
-        // verify timestamp
-        const timestamp: number = parseInt(timestampStr, 10)
-        if (isNaN(timestamp)) {
-            throw new PossessionParseError("timestamp was not a number")
-        }
+		// verify timestamp
+		const timestamp: number = parseInt(timestampStr, 10)
+		if (isNaN(timestamp)) {
+			throw new PossessionParseError("timestamp was not a number")
+		}
 
-        const now = from !== undefined ? from : Date.now()
-        const age = now - timestamp
-        const skew = ms(env.CERTIFICATE_REQUEST_TIME_SKEW_MAX)
-        if (age > skew) {
-            throw new PossessionParseError("timestamp too old")
-        }
-        if (age + skew < 0) {
-            throw new PossessionParseError("timestamp was from the future")
-        }
-        try {
-            // parse fingerprint
-            const fingerprint = parseFingerprint(fingerprintHex)
-            if (fingerprint === undefined) {
-                throw new PossessionParseError("fingerprint did not parse")
-            }
+		const now = from !== undefined ? from : Date.now()
+		const age = now - timestamp
+		const skew = ms(env.CERTIFICATE_REQUEST_TIME_SKEW_MAX)
+		if (age > skew) {
+			throw new PossessionParseError("timestamp too old")
+		}
+		if (age + skew < 0) {
+			throw new PossessionParseError("timestamp was from the future")
+		}
+		try {
+			// parse fingerprint
+			const fingerprint = parseFingerprint(fingerprintString)
+			if (fingerprint === undefined) {
+				throw new PossessionParseError("fingerprint did not parse")
+			}
 
-            // convert siganture from base64
-            const signature = Buffer.from(signatureBase64, "base64").toString()
+			// convert siganture from base64
+			const signature = Buffer.from(signatureBase64, "base64").toString()
 
-            // set values
-            this.timestamp = timestamp
-            try {
-                this.signature = parse(signature)
-            } catch (err) {
-                throw new PossessionParseError("proof of possession signature could not be parsed", err)
-            }
-            this.fingerprint = fingerprint
-            this.data = `${timestamp}.${fingerprintHex}`
-            this.signaturePubkey = parseKey(this.signature.publickey.toString())
-        } catch (err) {
-            switch (true) {
-                case (err instanceof FingerprintFormatError):
-                    throw new PossessionParseError("proof of possession fingerprint was an invalid format", err)
-                case (err instanceof DOMException):
-                    if (err.name == "InvalidCharacterError") {
-                        throw new PossessionParseError("proof of possession signature could not be parsed", err)
-                    }
-                default:
-                    throw err
-            }
-        }
-    }
+			// set values
+			this.timestamp = timestamp
+			try {
+				this.signature = parse(signature)
+			} catch (err) {
+				throw new PossessionParseError("proof of possession signature could not be parsed", err)
+			}
 
-    /**
-     *
-     * @returns true if signature is valid, false otherwise
-     */
-    async verify(namespace?: string): Promise<boolean> {
+			this.fingerprint = fingerprint
+			this.data = `${timestamp}.${fingerprintString}`
+			this.signaturePubkey = parseKey(this.signature.publickey.toString())
+
+			// ensure the provided fingerprint matches the signature public key
+			if (!this.fingerprint.matches(this.signaturePubkey)) {
+				throw new PossessionParseError("proof of possession fingerprint does not match signature public key")
+			}
+		} catch (err) {
+			switch (true) {
+				case (err instanceof FingerprintFormatError):
+					throw new PossessionParseError("proof of possession fingerprint was an invalid format", err)
+				case (err instanceof DOMException):
+					if (err.name == "InvalidCharacterError") {
+						throw new PossessionParseError("proof of possession signature could not be parsed", err)
+					}
+				default:
+					throw err
+			}
+		}
+	}
+
+	/**
+	 *
+	 * @returns true if signature is valid, false otherwise
+	 */
+	async verify(namespace?: string): Promise<boolean> {
 		return await verify(this.signature, this.data, { namespace: namespace || Namespace })
-    }
+	}
 
-    /**
-     *
-     * @param keys array of keys to verify finterprint against
-     * @returns true if the proof of possession fingerprint matches any of the provided keys, false otherwise
-     */
-    matches(key: Key): boolean
-    matches(...keys: Key[]): boolean
+	/**
+	 *
+	 * @param keys array of keys to verify finterprint against
+	 * @returns true if the proof of possession fingerprint matches any of the provided keys, false otherwise
+	 */
+	matches(key: Key): boolean
+	matches(...keys: Key[]): boolean
 
-    matches(...keys: Key[]): boolean {
-        if (keys.length === 1) {
-            return this.fingerprint.matches(keys[0])
-        }
+	matches(...keys: Key[]): boolean {
+		for (const key of keys) {
+			// confirm proof of possession fingerprint matches keys
+			if (!this.fingerprint.matches(key))
+				return false
 
-        for (const key of keys) {
-            // confirm proof of possession fingerprint matches keys
-            if (!this.fingerprint.matches(key))
-                return false
+			// also confirm key used to sign proof of possession matches
+			if (!this.signaturePubkey.fingerprint().matches(key))
+				return false
+		}
 
-            // also confirm key used to sign proof of possession matches
-            if (!this.signaturePubkey.fingerprint().matches(key))
-                return false
-        }
-
-        return true
-    }
+		return true
+	}
 }
 
 export class HostProofOfPossession extends ProofOfPossession {
-    matches(...keys: Key[]): boolean {
-        if (keys.length === 1) {
-            // must verify key and cert
-            return false
-        }
+	matches(key: Key): never
+	matches(...keys: Key[]): boolean
 
-        for (const key of keys) {
-            if (!this.fingerprint.matches(key))
-                return false
-        }
+	matches(...keys: Key[]): boolean {
+		if (keys.length === 1) {
+			throw new Error("must verify both public key and certificate")
+		}
 
-        return true
-    }
+		for (const key of keys) {
+			if (!this.fingerprint.matches(key))
+				return false
+			if (!this.signaturePubkey.fingerprint().matches(key))
+            	return false
+		}
+
+		return true
+	}
 }
